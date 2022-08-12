4e260e105a97db85ff4e9927b8314440d4cc0d79
Chaninder Rishi <chaninderrishi@Chaninders-MacBook-Pro.local>
2022-08-12 11:06:38 -0700

    updates

--- api/main.py
+++ api/main.py
@@ -5,9 +5,9 @@ app = FastAPI()
 
 @app.get("/ping")
 async def ping():
-    return "Hello!"
+    return "Hello!efef"
 
 
-if __name__=="__main__":
-    uvicorn.run(app, host='localhost', port=8000)
+#if __name__=="__main__":
+    #uvicorn.run(app, host='localhost', port=8000)
 
